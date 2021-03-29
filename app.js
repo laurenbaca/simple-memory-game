3f49e518a9e98b50254e9b59250d1d069235d1c2
Lauren Baca <laurenbaca.dev@gmail.com>
2021-03-29 12:42:58 -0700

    Simple memory game.

---
+++ app.js
@@ -0,0 +1,116 @@
+document.addEventListener('DOMContentLoaded', () => {
+  //card options
+  const cardArray = [
+    {
+      name: 'bulbasaur',
+      img: 'images/bulbasaur.png'
+    },
+    {
+      name: 'bulbasaur',
+      img: 'images/bulbasaur.png'
+    },
+    {
+      name: 'clefairy',
+      img: 'images/clefairy.png'
+    },
+    {
+      name: 'clefairy',
+      img: 'images/clefairy.png'
+    },
+    {
+      name: 'komala',
+      img: 'images/komala.png'
+    },
+    {
+      name: 'komala',
+      img: 'images/komala.png'
+    },
+    {
+      name: 'magikarp',
+      img: 'images/magikarp.png'
+    },
+    {
+      name: 'magikarp',
+      img: 'images/magikarp.png'
+    },
+    {
+      name: 'starly',
+      img: 'images/starly.png'
+    },
+    {
+      name: 'starly',
+      img: 'images/starly.png'
+    },
+    {
+      name: 'staryu',
+      img: 'images/staryu.png'
+    },
+    {
+      name: 'staryu',
+      img: 'images/staryu.png'
+    }
+  ]
+
+  cardArray.sort(() => 0.5 - Math.random())
+
+  const grid = document.querySelector('.grid')
+  const resultDisplay = document.querySelector('#result')
+  let cardsChosen = []
+  let cardsChosenId = []
+  let cardsWon = []
+
+  //create your board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      const card = document.createElement('img')
+      card.setAttribute('src', 'images/pokeball.jpg')
+      card.setAttribute('data-id', i)
+      card.addEventListener('click', flipCard)
+      grid.appendChild(card)
+    }
+  }
+
+  //check for matches
+  function checkForMatch() {
+    const cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+
+    if(optionOneId == optionTwoId) {
+      cards[optionOneId].setAttribute('src', 'images/pokeball.jpg')
+      cards[optionTwoId].setAttribute('src', 'images/pokeball.jpg')
+      alert('You have clicked the same image!')
+    }
+    else if (cardsChosen[0] === cardsChosen[1]) {
+      alert('You found a match!')
+      cards[optionOneId].setAttribute('src', 'images/white.png')
+      cards[optionTwoId].setAttribute('src', 'images/white.png')
+      cards[optionOneId].removeEventListener('click', flipCard)
+      cards[optionTwoId].removeEventListener('click', flipCard)
+      cardsWon.push(cardsChosen)
+    } else {
+      cards[optionOneId].setAttribute('src', 'images/pokeball.jpg')
+      cards[optionTwoId].setAttribute('src', 'images/pokeball.jpg')
+      alert('Sorry, try again.')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if  (cardsWon.length === cardArray.length/2) {
+      resultDisplay.textContent = 'Congratulations! You found them all!'
+    }
+  }
+
+  //flip your card
+  function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length ===2) {
+      setTimeout(checkForMatch, 500)
+    }
+  }
+
+  createBoard()
+})
